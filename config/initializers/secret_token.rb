283647011e66a547321173bb27246134bf4aa836
Sarah Allen <sarah@ultrasaurus.com>
2012-08-10 00:05:52 -0700

    blank rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colorswiper::Application.config.secret_token = '31b39ac757dc37b46ab0c2c7fed3b5666fa3bab1cfd87809061513585d540848524f36cbaa582e2546bff162ef094663e5641942e7aa25f0ae2969fd04bc7553'

471021dc0202e3ff2cd6bfdad60beb33cc28beec
Sarah Allen <sarah@ultrasaurus.com>
2012-08-10 02:14:21 -0700

    added simple tap action

--- app/assets/javascripts/patch.js
+++ app/assets/javascripts/patch.js
@@ -1,1 +1,22 @@
-Wheel.View.subclass('Patch');
+Wheel.View.subclass('Patch', {
+
+  // this is where we can add interactions
+  listen: function() {
+    this.$.on('tap', this.onTap.bind(this));
+  },
+
+  onTap: function(e) {
+    e.stopPropagation();
+    console.log('tap')
+
+    var red = Math.floor(Math.random()*10);
+    var green = Math.floor(Math.random()*10);
+    var blue = Math.floor(Math.random()*10);
+    this.color_text = "#" + red + green + blue
+    console.log(this.color_text)
+
+    this.$.css('background-color', this.color_text)
+  }
+
+
+});
